83585aa1d6f83f208124121414670eb1aac90ed6
Aditya Mahimkar <mahimkaradi@gmail.com>
2022-07-24 23:31:29 +0530

    required data populated

--- Frontend/src/app/admin-dashboard/admin-dashboard.component.ts
+++ Frontend/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -26,7 +26,7 @@ export class AdminDashboardComponent implements AfterViewInit {
   ];
 
   displayedColumns = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  dataSource = new MatTableDataSource<WorkItem>(ELEMENT_DATA);
 
   constructor() { }
 
@@ -44,32 +44,32 @@ export class AdminDashboardComponent implements AfterViewInit {
 
 }
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+export interface WorkItem {
+  workItemId:string,
+    userId:number,
+    itemName:string,
+    itemType:string,
+    itemDescription:string,
+    messageToRecipient:string,
+    capacity:number,
+    fromCountry:string,
+    destinationCountry:string ,
+    availableHarborLocations:string,
+    shippingDate:string
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+const ELEMENT_DATA : WorkItem[]= [
+  {
+    workItemId:"J2012",
+    userId: 10001,
+    itemName: "UPS",
+    itemType:"Computer Hardware",
+    itemDescription:"Luminous Inverter battery Combo 1100+150AH",
+    messageToRecipient:"Please confirm once recived.",
+    capacity:7500,
+    fromCountry:"India" ,
+    destinationCountry:"Singapore" ,
+    availableHarborLocations: "Tanjong Pagar",
+    shippingDate:'12-Aug-20'
+},
 ];
